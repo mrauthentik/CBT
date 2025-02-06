52ad7b9cc36ef2726ef31266866785bed9022ec8
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-06 12:08:40 +0100

    Add routing and user sign-up functionality with Home and SignUpPage components

--- CBT/src/App.tsx
+++ CBT/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Home from './Components/Home'
+import SignUpPage from './Components/UserAuth/SignUpPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-    <>
-     <h1>welcome</h1>
-    </>
+   <Router>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signup' element={<SignUpPage />} />
+    </Routes>
+   </Router>
   )
 }
 
