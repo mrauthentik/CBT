31b647e273f4d9a6c2aba5973e43898fb3fe014b
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-06 14:25:21 +0100

    Add SignInPage route and link to SignUpPage for user authentication

--- CBT/src/App.tsx
+++ CBT/src/App.tsx
@@ -5,6 +5,7 @@ import LandingPage from './pages/LandingPage'; // Import your LandingPage compon
 import './App.css'
 import Home from './Components/Home'
 import SignUpPage from './Components/UserAuth/SignUpPage'
+import SignInPage from './Components/UserAuth/SignInPage';
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
         <Route path="/" element={<LandingPage />} /> {/* Route for LandingPage */}
         <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
       </Routes>
     </BrowserRouter>
   );
