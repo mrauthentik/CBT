b0648b7611bc90219518266231083e17e3ac3c52
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-08 15:00:41 +0100

    feat: implement ExamPage component to fetch and display questions based on courseId; add addQuestions utility for adding sample questions

--- CBT/src/App.tsx
+++ CBT/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { useEffect } from 'react';
+// import addQuestions from './Components/Dashboard/addQuestions';
 
 import './App.css'
 import router from './Routes/Router';
 
 const App: React.FC = () => {
+  useEffect(()=>{
+    // addQuestions()
+  },[])
   return (
-    <RouterProvider  router={router}/>
+    <div>
+      {/* <p> Question data added</p> */}
+      <RouterProvider  router={router}/>
+
+    </div>
   );
 }
 
