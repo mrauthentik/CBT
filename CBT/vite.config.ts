0255842e600bde504497e399f7f89c2996450a3e
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-08 09:51:16 +0100

    feat: configure PostCSS in Vite for Tailwind CSS integration

--- CBT/vite.config.ts
+++ CBT/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import postcssConfig from './postcss.config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
+    css: {
+      postcss,
+    }
 })
