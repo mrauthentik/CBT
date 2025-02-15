0bd97ececb5af20bc765153cceaf5fb5da43a7cc
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-15 17:49:15 +0100

    Add Google Generative AI integration and remove unused code; update dependencies

--- CBT/src/AI.ts
+++ CBT/src/AI.ts
@@ -1,31 +1,31 @@
-// import { useEffect } from 'react'
+// // import { useEffect } from 'react'
 
-import { gemini20Flash, googleAI} from '@genkit-ai/googleai'
-import {genkit} from 'genkit'
+// import { gemini20Flash, googleAI} from '@genkit-ai/googleai'
+// import {genkit} from 'genkit'
 
 
-const ai  = genkit ({
-    plugins: [googleAI()],
-    model:gemini20Flash,
-})
+// const ai  = genkit ({
+//     plugins: [googleAI()],
+//     model:gemini20Flash,
+// })
 
-interface GenerateTextResponse {
-    text: string;
-}
-
-// interface GenerateTextError {
-//     message: string;
+// interface GenerateTextResponse {
+//     text: string;
 // }
 
-const generateText = async (prompt: string): Promise<string> => {
-    try {
-        const { text }: GenerateTextResponse = await ai.generate(prompt);
-        return text;
-        console.log(text);
-    } catch (error:unknown) {
-        console.log(error);
-        return "Error generating response";
-    }
-}
+// // interface GenerateTextError {
+// //     message: string;
+// // }
 
-export default generateText
+// const generateText = async (prompt: string): Promise<string> => {
+//     try {
+//         const { text }: GenerateTextResponse = await ai.generate(prompt);
+//         return text;
+//         console.log(text);
+//     } catch (error:unknown) {
+//         console.log(error);
+//         return "Error generating response";
+//     }
+// }
+
+// export default generateText
--- CBT/src/Components/Dashboard/UserInfo.tsx
+++ CBT/src/Components/Dashboard/UserInfo.tsx
@@ -1,45 +1,73 @@
+import { useEffect } from 'react';
 import SideBar from '../SideBar'
-import { model } from '../firebase'
+// import { model } from '../firebase'
 // import {genkit} from 'genkit'
-import generateText from '../../AI';
-import { useEffect, useState } from 'react';
+// import generateText from '../../AI';
+import { GoogleGenerativeAI } from "@google/generative-ai"
+// import { useEffect, useState } from 'react';
 
 
 const UserInfo = () => {
-const [aiResponse, setAiResponse] = useState<string>('')
+// const [aiResponse, setAiResponse] = useState<string>('')
 
-useEffect (()=>{
-  const fetchAiResponse = async () => {
-    const response = await generateText('Hello, Gemeini!')
-    setAiResponse(response)
-  }
-  fetchAiResponse ()
-},[])
+//Gnerative AI initialization model
+const apiKey = import.meta.env.VITE_GOOGLE_AI_API_KEY;
+if (!apiKey) {
+  throw new Error("API key is not defined");
+}
+const genAI = new GoogleGenerativeAI(apiKey);
+const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"})
+
+useEffect(()=>{
 
 
-async function run() {
+const show = async ()=>{
   try{
 
-    const prompt = 'Write a story about Nigeria'
-    const result = await model.generateContent(prompt)
-    const response = result.response
-    const text = response.text()
-    console.log(text)
-  }catch(error)
-  {
-      console.log("Could not generate text :", error)
+    const result = await model.generateContent('About Nigeria')
+    console.log(result.response.text())
+  }catch (error:unknown){
+     console.log(error)
   }
-  // console.log("this is the Genkit: ",genkit)
 }
+show()
+}, [])
+// useEffect (()=>{
+//   const fetchAiResponse = async () => {
+//     const response = await fetch (
+//        "https://us-central1-cbt-auth-7fb1e.cloudfunctions.net/generateAIText"
+//     );
+//     const data = await response.json()
+//     setAiResponse(data.response)
+//   }
+//   fetchAiResponse()
+// },[])
+
+
+
+// async function run() {
+//   try{
+
+//     const prompt = 'Write a story about Nigeria'
+//     const result = await model.generateContent(prompt)
+//     const response = result.response
+//     const text = response.text()
+//     console.log(text)
+//   }catch(error)
+//   {
+//       console.log("Could not generate text :", error)
+//   }
+//   // console.log("this is the Genkit: ",genkit)
+// }
   return (
     <div className='user-info'>
         <SideBar />
       <div className='user-info-container'>
         <h3>Name:</h3>
         <h3>Email:</h3>
-        <button onClick={run}> Run AI</button>
+        {/* <button onClick={run}> Run AI</button> */}
         <h2>Genkit AI response</h2>
-        <p>{aiResponse}</p>
+        <p></p>
       </div>
     </div>
   )
