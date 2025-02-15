59ff5ef9a8cd0cbb716ed672a5bd1bef26d8a0a8
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-15 17:56:29 +0100

    Refactor UserInfo component to enable AI response display in a textarea; remove commented-out code and add user input state

--- CBT/src/Components/Dashboard/UserInfo.tsx
+++ CBT/src/Components/Dashboard/UserInfo.tsx
@@ -1,14 +1,15 @@
-import { useEffect } from 'react';
 import SideBar from '../SideBar'
 // import { model } from '../firebase'
 // import {genkit} from 'genkit'
 // import generateText from '../../AI';
 import { GoogleGenerativeAI } from "@google/generative-ai"
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 const UserInfo = () => {
-// const [aiResponse, setAiResponse] = useState<string>('')
+const [aiResponse, setAiResponse] = useState<string>('')
+const [userInput, setUserInput] = useState('');
+
 
 //Gnerative AI initialization model
 const apiKey = import.meta.env.VITE_GOOGLE_AI_API_KEY;
@@ -26,39 +27,14 @@ const show = async ()=>{
 
     const result = await model.generateContent('About Nigeria')
     console.log(result.response.text())
+    setAiResponse(result.response.text())
   }catch (error:unknown){
      console.log(error)
   }
 }
 show()
 }, [])
-// useEffect (()=>{
-//   const fetchAiResponse = async () => {
-//     const response = await fetch (
-//        "https://us-central1-cbt-auth-7fb1e.cloudfunctions.net/generateAIText"
-//     );
-//     const data = await response.json()
-//     setAiResponse(data.response)
-//   }
-//   fetchAiResponse()
-// },[])
 
-
-
-// async function run() {
-//   try{
-
-//     const prompt = 'Write a story about Nigeria'
-//     const result = await model.generateContent(prompt)
-//     const response = result.response
-//     const text = response.text()
-//     console.log(text)
-//   }catch(error)
-//   {
-//       console.log("Could not generate text :", error)
-//   }
-//   // console.log("this is the Genkit: ",genkit)
-// }
   return (
     <div className='user-info'>
         <SideBar />
@@ -67,7 +43,7 @@ show()
         <h3>Email:</h3>
         {/* <button onClick={run}> Run AI</button> */}
         <h2>Genkit AI response</h2>
-        <p></p>
+        <textarea name="" value={aiResponse} rows={10} cols={70} id=""></textarea>
       </div>
     </div>
   )
