7c31334ddce1995f27db3db9dd08abaaa4773432
theprogrammingprince <eragbele.paul@gmail.com>
2025-02-19 16:30:13 +0100

    tried to recreate the about us section from the inspo

--- CBT/src/pages/Hero.tsx
+++ CBT/src/pages/Hero.tsx
@@ -17,19 +17,27 @@ const Hero = () => {
 {/* About Code */}
 
 <div className="about">
-    <div className="img-overlay">
+      {/* Image Overlay Section */}
+      <div className="img-overlay">
         <div className="img-box"></div>
         <div className="img-container">
-
-   <img src={logo} alt=""  className='about-img'/>
+          <img src={logo} alt="About Image" className="about-img" />
         </div>
-   </div>
-     <div className="about-text">
-        <h2>ABOUT US____</h2>
-        <p>NEXA: NOUN Exam Experience Assistant</p>
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quisquam at rem beatae debitis, eos amet quis similique cumque mollitia ut dignissimos a accusantium ipsum ab laborum eveniet corporis! Quis!
-     </div>
-   </div>
+      </div>
+
+      {/* Text Section */}
+      <div className="about-text">
+        <h2>ABOUT US <span className="underline"></span></h2>
+        <p>
+          <strong>NEXA:</strong> NOUN Exam Experience Assistant
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quisquam at rem
+          beatae debitis, eos amet quis similique cumque mollitia ut dignissimos a
+          accusantium ipsum ab laborum eveniet corporis! Quis!
+        </p>
+      </div>
+    </div>
 
 
 
