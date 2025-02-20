ae940a6b2ce2485c8b3b1cc78a7d0f8975e583e8
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-20 15:34:47 +0100

    Add Firestore imports to AdminRegister component

--- CBT/src/Components/Admin/AdminRegister.tsx
+++ CBT/src/Components/Admin/AdminRegister.tsx
@@ -1,3 +1,5 @@
 import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import {auth,db} from '../firebase'
+import {doc, setDoc } from 'firebase/firestore';
+import { useNavigate } from "react-router-dom";
