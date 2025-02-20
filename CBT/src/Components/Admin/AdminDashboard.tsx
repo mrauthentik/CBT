04fb449fccae4b9811920c97b2319b8c2cc7f81f
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-20 15:53:06 +0100

    Add Firebase imports and useState hook in AdminDashboard component

--- CBT/src/Components/Admin/AdminDashboard.tsx
+++ CBT/src/Components/Admin/AdminDashboard.tsx
@@ -1,6 +1,9 @@
-
+import { useState } from "react"
+import { db } from "../firebase"
+import { addDoc, collection } from 'firebase/firestore';
 
 const AdminDashboard = () => {
+    const [question]
   return (
     <div>
       <h2>Admin Dashboard</h2>
