97a133d42d543919927e91c4b14f3d8d0f7117b1
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-20 15:38:31 +0100

    Implement registration handler in AdminRegister component

--- CBT/src/Components/Admin/AdminRegister.tsx
+++ CBT/src/Components/Admin/AdminRegister.tsx
@@ -5,9 +5,23 @@ import {doc, setDoc } from 'firebase/firestore';
 import { useNavigate } from "react-router-dom";
 
 const AdminRegister = ()=>{
-    
+
     const [email, setEmail] = useState('')
     const [password, setpassword] = useState('')
     const navigate = useNavigate()
+
+   const handleRegister = async ()=>{
+    try{
+        const register = await createUserWithEmailAndPassword (auth, email, password)
+        const user = register.user
+
+        
+    }catch(err:unknown){
+        console.log(err)
+    }
+
+   }
+
+
 
 }
