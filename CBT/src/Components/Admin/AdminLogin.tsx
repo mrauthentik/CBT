98be54b81ba8cf3262fba42e607469a34ba5dc7a
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-20 15:09:25 +0100

    Implement AdminLogin component with authentication and admin verification

---
+++ CBT/src/Components/Admin/AdminLogin.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {auth, db} from '../firebase'
+import {doc, getDoc} from '../firebase/firestore'
+import { useNavigate } from "react-router-dom";
+
+
+const AdminLogin = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+
+
+    const handleLogin = async () =>{
+        try{
+            const useCredential = await signInWithEmailAndPassword(auth, email, password)
+            const user = useCredential.user
+
+            //Code to verify if user is an admin
+            const adminRef = doc(db, "admin", user.email || "")
+            const adminSnap = await getDoc(adminRef)
+
+            if(adminRef.exists()){
+                navigate('/admin')
+            }else{
+                console.log("Access Denied: You are not an Admin")
+            }
+
+        }catch(error:unknown){
+         console.log(error)
+        }
+    }
+}
+
+export default AdminLogin
